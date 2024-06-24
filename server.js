8c6d68ac938206d2806a40fd01a0c2a5c40a9e83
marco.alvarador <marco.alvarador@gmail.com>
2024-06-24 22:48:50 +0000

    Mas actualizaciones en el archivo server

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+//importacion de la ruta para controlar el flujo
+const routerAPI = require("./router/index.js");
+//Llamado de express para iniciar el servidor
 const express = require('express');
+//gestion de directorios en el servidor
+const fs = require('fs');
+//viriable para el envio de informacion 
+const { send } = require('process');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
